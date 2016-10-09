032d3b531e3d25bb1a0dada23e3e076c68e9f8ba
jitender-axovel <jitender.axovel@gmail.com>
2016-10-09 17:36:43 +0530

    added functions to login through twitter

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,14 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Session;
+
+use Socialite;
+
+use Twitter;
+
+use App\User;
+
 class LoginController extends Controller
 {
     /*
@@ -25,7 +33,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -35,5 +43,94 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        $user = Socialite::driver($provider)->user();
+
+        Session::put('oauth_request_token', $user->token);
+        Session::put('oauth_request_token_secret', $user->tokenSecret);
+
+        $data = array_intersect_key((array)$user, User::$updatable);
+
+        $user = User::firstOrCreate($data);
+
+        // your SIGN IN WITH TWITTER  button should point to this route
+        $sign_in_twitter = true;
+        $force_login = false;
+
+        // Make sure we make this request w/o tokens, overwrite the default values in case of login.
+        Twitter::reconfig(['token' => '', 'secret' => '']);
+        $token = Twitter::getRequestToken(route('twitter.callback'));
+
+        if (isset($token['oauth_token_secret']))
+        {
+            $url = Twitter::getAuthorizeURL($token, $sign_in_twitter, $force_login);
+
+            Session::put('oauth_state', 'start');
+            Session::put('oauth_request_token', $token['oauth_token']);
+            Session::put('oauth_request_token_secret', $token['oauth_token_secret']);
+
+            // $user->token;
+
+            auth()->login($user);
+
+            return redirect('/');
+        }
+
+        return Redirect::route('twitter.error');
+    }
+
+    public function twitterCallback()
+    {
+        if (Session::has('oauth_request_token'))
+        {
+            $request_token = [
+                'token'  => Session::get('oauth_request_token'),
+                'secret' => Session::get('oauth_request_token_secret'),
+            ];
+
+            Twitter::reconfig($request_token);
+
+            $oauth_verifier = false;
+
+            if (Input::has('oauth_verifier'))
+            {
+                $oauth_verifier = Input::get('oauth_verifier');
+            }
+
+            // getAccessToken() will reset the token for you
+            $token = Twitter::getAccessToken($oauth_verifier);
+
+            if (!isset($token['oauth_token_secret']))
+            {
+                return redirect('/')->with('flash_error', 'We could not log you in on Twitter.');
+            }
+
+            $credentials = Twitter::getCredentials();
+
+            if (is_object($credentials) && !isset($credentials->error))
+            {
+                Session::put('access_token', $token);
+
+                return redirect('/')->with('flash_notice', 'Congrats! You\'ve successfully signed in!');
+            }
+        }
     }
 }
