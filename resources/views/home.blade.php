a9a5c0f5c4a7ce93de43c3cde152ba28b643c5fe
jitender-axovel <jitender.axovel@gmail.com>
2016-10-09 17:41:55 +0530

    updated home blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <!-- Jumbotron -->
+        <div class="jumbotron text-center">
+            <h1>Hello, WhiteBox Studio!</h1>
+            <p class="lead"></p>
+            @if(Auth::check())
+                <p><a class="btn btn-lg btn-success" href="javascript:void(0);" onclick="javascript:getTweets()" role="button">Get Latest Tweets</a></p>
+            @else
+                <p><a class="btn btn-lg btn-success" href="{{ url('auth/twitter') }}">Login to Continue</a></p>
+            @endif
+        </div>
+        @if(Auth::check())
+            <div id="tweeter-results"></div>
+        @endif
+    </div>
+</div>
+@endsection
+@section('footer-scripts')
+    <script type="text/javascript">
+        function getTweets()
+        {
+            $.ajax({
+                url: "{{ route('twitter.tweets') }}",
+                type: "GET",
+                success: function(data) {
+                    $('#tweeter-results').html(data);
+                }
+            })
+        }
+    </script>
+@endsection
