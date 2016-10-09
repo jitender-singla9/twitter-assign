aeda22270e1c6491503ac113f0abe81ab6aa09c8
jitender-axovel <jitender.axovel@gmail.com>
2016-10-09 17:39:30 +0530

    updated users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('email')->nullable();
+            $table->string('password')->nullable();
+            $table->string('avatar');
             $table->rememberToken();
             $table->timestamps();
         });
